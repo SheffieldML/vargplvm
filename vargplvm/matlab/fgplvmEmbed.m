b65095201604ce2d96708cc4e4a9545858322c8d
Andreas <adamianou@gmail.com>
2013-07-23 12:04:23 +0100

    Added fgplvmEmbed.m (useful e.g. for initialisation)

---
+++ vargplvm/matlab/fgplvmEmbed.m
@@ -0,0 +1,57 @@
+function [X, sigma2, W, model] = fgplvmEmbed(Y, dims, varargin)
+% e.g. Y = sin(1:0.05:10)' * rand(1,10); Y = Y + randn(size(Y)).*0.1;
+
+iters = 200;
+initSNR = 100;
+display = 1;
+
+options = fgplvmOptions('fitc');
+options.numActive = min(100, size(Y,1));
+options.optimiser = 'scg';
+    
+if nargin > 3
+    options2 = varargin{1};
+    options.numActive = options2.numActive;
+    initSNR = options2.initSNR;
+    if length(varargin)>1 && ~isempty(varargin{2}), initVardistIters = varargin{2}; end
+    if length(varargin)>2 && ~isempty(varargin{3}), iters = varargin{3}; end
+    if length(varargin)>3 && ~isempty(varargin{3}), initSNR = varargin{4}; end
+    if length(varargin)>4 && ~isempty(varargin{4}), display = varargin{5}; end
+end
+
+latentDim = dims;
+d = size(Y, 2);
+
+model = fgplvmCreate(latentDim, d, Y, options);
+%
+if isfield(model, 'beta')
+    model.beta = 1/((1/initSNR * var(model.m(:))));
+    if model.beta < 1e-7
+        warning('Beta was too small... Setting beta to 1e-7')
+        model.beta = 1e-7;
+    elseif model.beta > 1e+7
+        warning('Beta was too big... Setting beta to 1e+7')
+        model.beta = 1e+7;
+    end
+end
+
+
+fprintf('#--- fgplvmEmbed from %d dims to %d dims, K=%d...\n',d,latentDim,options.numActive);
+
+
+if iters > 0
+    fprintf('\n  # fgplvmEmbed: Optimising for %d iters...\n',iters);
+    model = fgplvmOptimise(model, display, iters);
+end
+X = model.X;
+sigma2 = [];
+W = [];
+
+if isfield(model, 'beta')
+    finalSNR = var(model.m(:))*model.beta;
+    fprintf('# fgplvmEmbed complete... final SNR: %f\n\n',finalSNR);
+    if finalSNR < 10
+        warning('# fgplvmEmbed: final SNR is too low!! (%f) \n\n', finalSNR)
+    end
+end
+
