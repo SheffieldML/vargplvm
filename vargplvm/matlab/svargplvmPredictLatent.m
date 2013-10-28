cb922396e4adc0c58eafa5ded433eff9589cb4f3
Andreas <adamianou@gmail.com>
2013-10-28 14:04:50 +0000

    Added utility to compute approximate posterior for test points.

--- vargplvm/matlab/svargplvmPredictLatent.m
+++ vargplvm/matlab/svargplvmPredictLatent.m
@@ -1,14 +1,17 @@
 function [x_star_all, varx_star_all, mini] = ...
-    vargplvmPredictLatent(model, Yts, testInd, batchMode, iters, displayTestOpt, mini)
+    svargplvmPredictLatent(model, Yts, testInd, obsMod, batchMode, iters, displayTestOpt, mini)
 
-% VARGPLVMPREDICTLATENT Given output observation(s) compute the approximate
-% posterior
-% DESC Given an output observed test point y* (or set of test points Y*)
-% compute the approximate posterior p(X* | Y*) for the corresponding latent
-% points.
+% SVARGPLVMPREDICTLATENT
 %
-% ARG model: The trained vargplvm model
-% ARG Yts: The set of outputs observations
+% DESC Given one ore more output observations find the corresponding latent
+% point from the approximate posterior.
+% Similar to vargplvmPredictLatent but this function accepts more than
+% one given modalities (if only one given it should be identical to
+% vargplvmPredictLatent).
+%
+% ARG model: The trained svargplvm (MRD) model
+% ARG Yts: The set of outputs observations. It's a cell array with one cell
+% being the matrix of observations for one modality
 % ARG testInd: The indices of Yts to do inference for (default: all)
 % ARG batchMode: If set to true, then if N* points are given in Yts, only
 % one optimisation will be done in the N*-dimensional space. Otherwise, N*
@@ -22,28 +25,42 @@ function [x_star_all, varx_star_all, mini] = ...
 % Xtrain(mini(i), :). If this vector is not given then this vector is
 % computed here in the default way.
 %
-%
-% SEEALSO: 
+% SEEALSO : vargplvmPredictLatent
 %
 % COPYRIGHT : Andreas C. Damianou, 2013
 %
 % VARGPLVM
 
 if nargin < 2 || isempty(model) || isempty(Yts), error('At least 2 arguments needed!'); end
-if nargin < 3 || isempty(testInd), testInd = 1:size(Yts,1); end
-if nargin < 4 || isempty(batchMode), batchMode = false; end
-if nargin < 5 || isempty(iters), iters = model.globalOpt.reconstrIters; end
-if nargin < 6 || isempty(displayTestOpt), displayTestOpt = false; end
-if nargin < 7, mini = []; end
+if nargin < 3 || isempty(testInd), testInd = 1:size(Yts{1},1); end
+if nargin < 4 || isempty(obsMod), obsMod = 1:length(Yts); end
+if nargin < 5 || isempty(batchMode), batchMode = false; end
+if nargin < 6 || isempty(iters), iters = model.globalOpt.reconstrIters; end
+if nargin < 7 || isempty(displayTestOpt), displayTestOpt = false; end
+if nargin < 8, mini = []; end
 
 miniGiven = ~isempty(mini);
 
+for jj = obsMod
+    Yts{jj} = Yts{jj}(testInd, :);
+end
+
+if ~isfield(model, 'testModalities')
+    model.testModalities = obsMod;
+end
+
 if batchMode
-    y_star = Yts(testInd,:);
     if ~miniGiven
+        y_star = [];
+        tmpYtr = [];
+        for jj = obsMod
+            y_star = [y_star Yts{jj}];
+            tmpYtr = [tmpYtr model.comp{jj}.y];
+            
+        end
         mini = NaN(1,length(testInd));
-        for i=1:length(testInd)
-             dst = dist2(y_star(i,:), model.y);
+        for i=1:length(mini)
+            dst = dist2(y_star(i,:), tmpYtr);
             [~, mini(i)] = min(dst);
         end
     end
@@ -51,7 +68,7 @@ if batchMode
     vardistx = vardistCreate(model.vardist.means(mini, :), model.q, 'gaussian');
     vardistx.covars = model.vardist.covars(mini, :);
     model.vardistx = vardistx;
-    [x_star_all, varx_star_all] = vargplvmOptimisePoint(model, vardistx, y_star, true, iters);
+    [x_star_all, varx_star_all] = svargplvmOptimisePoint(model, vardistx, Yts, true, iters);
 else
     x_star_all = zeros(length(testInd), model.q);
     varx_star_all = zeros(length(testInd), model.q);
@@ -60,20 +77,27 @@ else
     end
     pb = myProgressBar(length(testInd), min(length(testInd),20));
     for i=1:length(testInd)
-        pb = myProgressBar(pb,i);
-        curInd = testInd(i);
-        y_star = Yts(curInd,:);
+        pb = myProgressBar(pb,i);  
         if ~miniGiven
-            dst = dist2(y_star, model.y);
+            tmpYtr = [];
+            y_star = [];
+            for jj=1:obsMod
+                y_star = [y_star Yts{jj}(i, :)];
+                tmpYtr = [tmpYtr model.comp{jj}.y];
+            end
+            dst = dist2(y_star, tmpYtr);
             [~, mini(i)] = min(dst);
         end
         model.mini = mini(i);
         vardistx = vardistCreate(model.vardist.means(mini(i),:), model.q, 'gaussian');
         vardistx.covars = model.vardist.covars(mini(i),:);
         model.vardistx = vardistx;
-        
+        yts_cur = cell(1, length(Yts));
+        for jj = obsMod
+            yts_cur{jj} = Yts{jj}(i,:);
+        end
         % Find p(X_* | Y_*) which is approximated by q(X_*)
-        [x_star, varx_star] = vargplvmOptimisePoint(model, vardistx, y_star, displayTestOpt, iters);
+        [x_star, varx_star] = svargplvmOptimisePoint(model, vardistx, yts_cur, displayTestOpt, iters);
         x_star_all(i,:) = x_star;
         varx_star_all(i,:) = varx_star;
     end
