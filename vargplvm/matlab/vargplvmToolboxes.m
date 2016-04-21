794433a8318f267bfe94c77b2730491a7c6b57ca
Neil Lawrence <N.Lawrence@sheffield.ac.uk>
2016-04-21 02:32:32 -0400

    Update to vargplvm

--- vargplvm/matlab/vargplvmToolboxes.m
+++ vargplvm/matlab/vargplvmToolboxes.m
@@ -1,13 +1,5 @@
 % VARGPLVMTOOLBOXES Load in the relevant toolboxes for variational gplvm.
 
 importLatest('netlab');
-importLatest('prior');
-importLatest('optimi');
-importLatest('datasets');
-importLatest('mltools');
-importLatest('kern');
-importLatest('ndlutil');
-importLatest('mocap');
-importLatest('gp');
-importLatest('noise');
+importLatest('gpmat');
 
